edbd776d0d95a089b8ca23dff14c2bfac93df8c1
Julieta <109936071+Julis19@users.noreply.github.com>
2023-09-10 04:19:30 -0300

    Add files via upload

---
+++ ventana.js
@@ -0,0 +1,21 @@
+/*<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+    <script>
+        function mialerta() {  
+            alert("¡Hola..., Soy un mensaje de alerta...!");
+        } 
+    </script>
+
+</head>
+<body>
+
+    <input type="button" onclick="mialerta();" value="Clic para ver el mensaje de alerta"/>
+    
+</body>
+</html>
